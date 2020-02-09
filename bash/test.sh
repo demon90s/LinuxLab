356f7d40f5dc80ac3feecaffe88c57006984b583
diwen <demon90s@163.com>
2020-02-09 14:37:48 +0800

    commit

--- bash/test.sh
+++ bash/test.sh
@@ -26,4 +26,21 @@ fi
 # ! 取反
 [ ! -n "Hi" ] && echo "string empty" || echo "string not empty"
 
+# 自定义函数
+is_odd()
+{
+    local n=$(($1 % 2))
+    if [ "$n" = "1" ]; then
+        return 0    # 返回0表示真
+    else
+        return 1    # 表示假
+    fi
+}
+
+if is_odd 8; then
+    echo "is odd num"
+else
+    echo "is not odd num"
+fi
+
 exit 0
